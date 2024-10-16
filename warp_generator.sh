3984b7a41948745223f88e4e463291ea947790b0
Andrey Tomashev <git config --global user.name Andrey>
2024-10-16 22:50:46 +0300

    FIX url

--- warp_generator.sh
+++ warp_generator.sh
@@ -50,5 +50,5 @@ echo "${conf}"
 [ -t 1 ] && echo "########### КОНЕЦ КОНФИГА ###########"
 
 conf_base64=$(echo -n "${conf}" | base64 -w 0)
-echo "Скачать конфиг файлом: https://immalware.github.io/downloader.html?filename=DISCORD.conf&content=${conf_base64}"
+echo "Скачать конфиг файлом: https://drysua.github.io/downloader.html?filename=DISCORD.conf&content=${conf_base64}"
 echo -e "\n"
